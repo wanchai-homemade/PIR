54a466b0acf97f5adecef3d2203ee27e53fc7038
wanchai-homemade <56559025+wanchai-homemade@users.noreply.github.com>
2019-10-15 10:36:30 +0700

    Add files via upload

---
+++ pir/pir.ino
@@ -0,0 +1,36 @@
+
+int pirPin = D1;
+int relayPin1 = D5;
+int relayPin2 = D6;
+int pirValue;
+
+
+void setup()
+{
+  Serial.begin(115200);
+ 
+  pinMode(pirPin, INPUT);
+  pinMode(relayPin1, OUTPUT);
+  pinMode(relayPin2, OUTPUT);
+}
+
+void pirSensor()
+{
+  pirValue = digitalRead(pirPin);
+  
+}
+
+void loop()
+{
+  
+  pirSensor();
+  if (pirValue == HIGH)
+  {
+    digitalWrite(relayPin1, LOW);
+    digitalWrite(relayPin2, LOW);
+  }
+  
+  else
+  digitalWrite(relayPin1, HIGH);
+  digitalWrite(relayPin2, HIGH); 
+}
